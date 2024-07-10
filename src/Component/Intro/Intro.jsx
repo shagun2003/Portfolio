064a6abbb8a1f539fc505a4e844d349975c1cd2c
Shagun_Singh <134138604+shagun2003@users.noreply.github.com>
2024-07-10 10:52:58 +0530

    Update Intro.jsx

--- src/Component/Intro/Intro.jsx
+++ src/Component/Intro/Intro.jsx
@@ -1,26 +1,26 @@
+import "./Intro.css";
+import twotree from "../../ima/twotree.png";
 
-import "./Intro.css"
-import twotree from "../../ima/twotree.png" 
 export const Intro = () => {
   return (
-    <section className={container}>
-      <div className={content}>
-        <h1 className={title}>Hi, I'm Ada</h1>
-        <p className={description}>
+    <section className="container">
+      <div className="content">
+        <h1 className="title">Hi, I'm Ada</h1>
+        <p className="description">
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={contactBtn}>
+        <a href="mailto:myemail@email.com" className="contactBtn">
           Contact Me
         </a>
       </div>
       <img
         src={twotree}
         alt="Hero image of me"
-        className={heroImg}
+        className="heroImg"
       />
-      <div className={topBlur} />
-      <div className={bottomBlur} />
+      <div className="topBlur" />
+      <div className="bottomBlur" />
     </section>
-  )
-}
+  );
+};
